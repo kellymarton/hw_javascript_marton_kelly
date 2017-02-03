571e5edbd5153f1687080bd38a0b45114da895ed
Kelly Marton <kellymmarton@gmail.com>
2017-02-03 11:11:58 -0500

    add presidents array

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var presidents = [
+    {firstName: "Donald", lastName: "Trump", termLength: "Two weeks", party: "Republican", yearsOfPresidency: "2017"},
+    {firstName: "Barack", lastName: "Obama", termLength: "8 years", party: "Democratic", yearsOfPresidency: "2009-2017"},
+    {firstName: "George W.", lastName: "Bush", termLength: "8 years", party: "Republican", yearsOfPresidency: "2001-2009"},
+    {firstName: "Bill", lastName: "Clinton", termLength: "8 years", party: "Democratic", yearsOfPresidency: "1993-2001"},
+    {firstName: "George", lastName: "Bush", termLength: "4 years", party: "Republican", yearsOfPresidency: "1989-1993"}
+];
+
+console.log(presidents[3]);
+    
+    
